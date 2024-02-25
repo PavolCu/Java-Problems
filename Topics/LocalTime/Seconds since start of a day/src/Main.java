ec1133129293f011b782a1ebcfdf2f293c1b71aa
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-25 18:40:09 +0100

    another solution seconds since start of a day

--- Topics/LocalTime/Seconds since start of a day/src/Main.java
+++ Topics/LocalTime/Seconds since start of a day/src/Main.java
@@ -1,19 +1,6 @@
-import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int totalSeconds = scanner.nextInt();
-        scanner.close();
-
-        int hours = totalSeconds / 3600;
-        int minutes = (totalSeconds % 3600) / 60;
-        int seconds = totalSeconds % 60;
-
-        if (seconds == 0) {
-            System.out.printf("%02d:%02d\n", hours, minutes);
-        } else {
-            System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
-        }
+        System.out.print(java.time.LocalTime.ofSecondOfDay(new java.util.Scanner(System.in).nextInt()));
     }
 }
