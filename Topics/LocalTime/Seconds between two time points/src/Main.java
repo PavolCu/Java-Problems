f4ec666237582ddd326f45d36a8b3da95915c782
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-25 18:50:00 +0100

    seconds between two time points

--- Topics/LocalTime/Seconds between two time points/src/Main.java
+++ Topics/LocalTime/Seconds between two time points/src/Main.java
@@ -1,10 +1,19 @@
 import java.time.LocalTime;
+import java.time.Duration;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // write your code here
+        LocalTime time1 = LocalTime.parse(scanner.nextLine());
+        LocalTime time2 = LocalTime.parse(scanner.nextLine());
+        scanner.close();
+
+        System.out.println(getSecondsBetween(time1, time2));
+    }
+
+    public static long getSecondsBetween(LocalTime time1, LocalTime time2) {
+        Duration duration = Duration.between(time1, time2);
+        return Math.abs(duration.getSeconds());
     }
 }
