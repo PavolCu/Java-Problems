f235115341935faf93985c611083cab0cbabf3b3
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-25 18:25:57 +0100

    seconds since start of a day

--- Topics/LocalTime/Seconds since start of a day/src/Main.java
+++ Topics/LocalTime/Seconds since start of a day/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int totalSeconds = scanner.nextInt();
+        scanner.close();
+
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        if (seconds == 0) {
+            System.out.printf("%02d:%02d\n", hours, minutes);
+        } else {
+            System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+        }
     }
 }
