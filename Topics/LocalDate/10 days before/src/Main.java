8a06f45aa4b53f1e382e692b2868888d7f33b332
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-05-07 19:48:50 +0200

    10 days before

--- Topics/LocalDate/10 days before/src/Main.java
+++ Topics/LocalDate/10 days before/src/Main.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String inputDate = scanner.nextLine();
+        LocalDate date = LocalDate.parse(inputDate);
+        LocalDate tenDaysBefore = date.minusDays(10);
+        System.out.println(tenDaysBefore);
     }
 }
