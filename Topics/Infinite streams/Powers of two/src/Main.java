0270ef85da306f23b018009232f6b11731ad95ce
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-05-09 22:27:45 +0200

    Power of two

--- Topics/Infinite streams/Powers of two/src/Main.java
+++ Topics/Infinite streams/Powers of two/src/Main.java
@@ -5,6 +5,6 @@ import java.util.stream.Stream;
 class StreamUtils {
 
     public static Stream<Integer> generateStreamWithPowersOfTwo(int n) {
-        return Stream.empty(); // replace it with your code
+        return Stream.iterate(1, i -> i * 2).limit(n); // replace it with your code
     }
 }
